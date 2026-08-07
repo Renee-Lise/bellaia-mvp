4e58c800c10b7e88ec13a5e1a00803fc5e798a9f
Renee-Lise <bella.odyssee@hotmail.com>
2026-08-07 00:47:47 -0300

    Add files via upload

---
+++ src/modules/index.ts
@@ -0,0 +1,5 @@
+// src/modules/payments/index.ts
+export { default } from './PaymentCenter';
+export { default as PaymentCenter } from './PaymentCenter';
+export * from './paymentTypes';
+export * from './paymentApi';
